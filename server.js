97b5093ee1a2f69c73116919c8315d62aa055912
Dooshima <danielladoosh@gmail.com>
2021-02-24 13:40:18 +0100

    am not sure

--- server.js
+++ server.js
@@ -14,11 +14,9 @@ app.get('/api', (req,res) => {
 app.use(cors());
 
 app.get('/*',(req,res) => {
-  
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
   res.sendFile(path.join(__dirname , 'build', 'index.html'))
 })
